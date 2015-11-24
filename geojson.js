4334937a60f7deccdd6bc7ecb07f77bf4bb753ae
Chase Gruber <chasegruber@gmail.com>
2015-11-24 09:38:08 -0500

    commit project

---
+++ geojson.js
@@ -0,0 +1,137 @@
+﻿var locations = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.50613403320311,
+          41.61749568924246
+        ]
+      },
+      "properties": {
+        "Town": "Mashpee",
+        "Date": "July 2",
+        "Location": "Mashpee High School",
+        "Tips": "The festivities begin at 5 p.m. with a whole evening of food, rides, games, music, and lots more.",
+        "Class":"blue"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.5972114,41.544958
+        ]
+      },
+      "properties": {
+        "Town": "Falmouth",
+        "Date": "July 4",
+        "Location": "Falmouth Heights Beach",
+        "Tips": "Do you have a portable radio? Be sure to bring it with you, and tune to PIXY 103 (102.9 FM) to hear the music that accompanies the fireworks.",
+        "Class":"green"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.26031494140625,
+          41.636999392019725
+        ]
+      },
+      "properties": {
+        "Town": "Hyannis",
+        "Date": "July 4",
+        "Location": "Lewis Bay",
+        "Tips": "The best viewing spots are the beaches around Hyannis Harbor and on the West Yarmouth beaches along Lewis Bay. Pack a picnic basket for dinner and stake out your spot in late afternoon. Or have a bite to eat at a nearby restaurant, then walk to the beach for the fireworks show. Don't forget your cover-up clothes. It gets chilly on the beach after sunset, even in July!",
+        "Class":"green"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.17791748046875,
+          42.039094188385945
+        ]
+      },
+      "properties": {
+        "Town": "Provincetown",
+        "Date": "July 4",
+        "Location": "Provincetown Harbor",
+        "Tips": "For a very special experience, watch the fireworks from onboard a ship in the Harbor. On July 4th, <a target='_blank' href='http://whalewatch.com/'>Dolphin Fleet</a> offers whale watch plus fireworks cruises at 5 p.m., 5:30 p.m. and 6 p.m., as well as a fireworks-only cruise at 8 p.m. These cruises are very popular, so reserve your spot early.",
+        "Class":"yellow"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.653028,41.737030
+        ]
+      },
+      "properties": {
+        "Town": "Onset",
+        "Tips": "The town of Onset is on the \"mainland\" side of the Cape Cod Canal, just over the bridge. It's an absolutely gorgeous little town, and well worth a visit!",
+        "Location": "Onset Beach and Bay",
+        "Date": "July 4",
+        "Class":"yellow"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.008993,41.800530
+        ]
+      },
+      "properties": {
+        "Town": "Orleans",
+        "Date": "July 5",
+        "Location": "Rock Harbor",
+        "Class":"blue",
+        "Tips": "You can also see the Rock Harbor fireworks display from Cape Cod Bay beaches in Brewster and Eastham. They'll be off at a distance, but still fun to see."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.1247883,41.3009105
+        ]
+      },
+      "properties": {
+        "Town": "Nantucket",
+        "Date": "July 4",
+        "Location": "Jetties Beach",
+        "Class":"blue",
+        "Tips": "Fireworks are set off from a barge at Jetties Beach at 9:00 p.m. on July 4th."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.499267578125,
+          41.39844522006508
+        ]
+      },
+      "properties": {
+        "Town": "Martha's Vineyard",
+        "Tips": "The Edgartown parade starts at exactly 5 p.m. on July 4th and the Independence Day fireworks start at dusk on the harbor.",
+        "Location": "Edgartown",
+        "Class":"yellow",
+        "Date": "July 4"
+      }
+    }
+  ]
+}
